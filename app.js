a5c7cd55e3d6ecea15053aecc9508d53614c1bca
alexdietz1988 <alex.dietz@gmail.com>
2022-03-26 14:33:18 -0400

    Tried organizing workplaces by open/closed status

--- app.js
+++ app.js
@@ -6,8 +6,43 @@ const BPL = [
     [09, 20],
     [09, 17],
     [09, 17],
-    'Boston Public Library',
+    'The Boston Public Library',
 ]
+
+const CPL = [
+    [13, 17],
+    [09, 21],
+    [09, 21],
+    [09, 21],
+    [09, 21],
+    [09, 17],
+    [09, 17],
+    'The Cambridge Public Library',
+]
+
+const starbucksKendall = [
+    [06, 20], // It actually opens at 5:30am
+    [05, 20],
+    [05, 20],
+    [05, 20],
+    [05, 20],
+    [05, 20],
+    [09, 17], // It actually opens at 5:30am
+    'Starbucks Kendall',
+]
+
+const caffeNero = [
+    [07, 17],
+    [07, 17],
+    [07, 17],
+    [07, 17],
+    [07, 17],
+    [07, 17],
+    [07, 17],
+    'Caffe Nero',
+]
+
+const workplaces = [BPL, CPL, starbucksKendall, caffeNero];
 
 let d = new Date();
 let currentWeekday = d.getDay()
@@ -19,19 +54,51 @@ function weekdayInEnglish(weekdayNum) {
     return weekdays[weekdayNum]
     }
 
-function TimeInEnglish(hours, minutes) {
-    return `${hours}:${minutes}`
+function timeInEnglish(hours) {
+    if (hours > 12) return `${hours - 12}pm`;
+    else return `${hours}am`;
 }
-
-console.log(`Right now it's ${weekdayInEnglish(currentWeekday)} at ${TimeInEnglish(currentHour, currentMinutes)}`)
 
 function isItOpen(arr) {
     if (currentHour > arr[currentWeekday][0] && currentHour < arr[currentWeekday][1]) {
-        return true
-    }
-    return false
+        return true;
+    };
+    return false;
 }
 
+function getMessage(arr) {
+    if (isItOpen(arr) === true) {
+        return `${arr[7]} is open! It closes today at ${timeInEnglish(arr[currentWeekday][1])}.`
+    }
+    else {
+        return `${arr[7]} is currently closed.`
+    }
+}
 
+console.log(getMessage(workplaces[1]))
 
-console.log(isItOpen(BPL))
+const rightNow = document.createElement('p')
+rightNow.innerHTML = `Right now it's ${weekdayInEnglish(currentWeekday)} at ${timeInEnglish(currentHour)}.`
+document.body.appendChild(rightNow)
+
+let openSec = document.querySelector("#open")
+
+function messageForOpen() {
+    for (let i = 0; i < workplaces.length; i++) {
+        if (isItOpen(workplaces[i]) === true) {
+            message = document.createElement('p');
+            message.innerHTML = getMessage(workplaces[i]);
+            console.log(message)
+
+            document.openSec.appendChild(message);
+        }
+    }
+}
+
+function messageForClosed() {
+    for (let i = 0; i < workplaces.length; i++) {
+        if (isItOpen(workplaces[i] === false)) appendMessage(workplaces[i])
+    }
+}
+
+messageForOpen()
